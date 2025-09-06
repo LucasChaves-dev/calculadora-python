6052611800a268b59e0d60e90453e9de13d76d6d
Lucas Chaves <texhop7@gmail.com>
2025-09-06 16:15:13 -0300

    Add files via upload

---
+++ velocidade_media.py
@@ -0,0 +1,6 @@
+print("simulador de computador de bordo")
+distancia = float(input("informe a distancia percorrida: "))
+tempo = float(input("informe o tempo gasto na viagem: "))
+
+velocidade_media = distancia/tempo
+print(f"sua velocidade media foi de: {velocidade_media:.2f} km/h")
